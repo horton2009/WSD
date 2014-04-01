a4a6bbed3ac0490630d631d04ec25447ca19bc25
horton2009 <horton.hu.5@gmail.com>
2014-04-01 22:14:00 +0800

    Last Vertion

--- src/NB.py
+++ src/NB.py
@@ -4,6 +4,7 @@
 from numpy import *
 import util
 import time
+import random
 from Feature_Extractor import Extractor
 
 class NaiveBayes(object):
@@ -74,7 +75,7 @@ class NaiveBayes(object):
                 returnVec[self.VocabList.index(word)] =returnVec[self.VocabList.index(word)] + 1
         return returnVec    
 
-    def learn(self):                            # trainMatrix :must be numpy.array, to +
+    def learn(self,smooth_rate):                            # trainMatrix :must be numpy.array, to +
         print "\nLearning ..."
         trainMat=[]
         for doc in self.docList:                # 
@@ -86,7 +87,7 @@ class NaiveBayes(object):
         for c in set(self.classVec):
             self.PcMap[c] = (self.classVec.count(c))/float(numTrainDocs)
             #////////////////////////////////////////////////////////////////////
-            self.Pc_xMap[c] =  array([0.14]*numWords)    
+            self.Pc_xMap[c] =  array([smooth_rate]*numWords)    
             #////////////////////////////////////////////////////////////////////
         ctotal = {}                             # record the total none zero feature number of samples for each class
         for i in range(numTrainDocs):
@@ -96,16 +97,19 @@ class NaiveBayes(object):
 
         for c in set(self.classVec):
             self.Pc_xMap[c] =  self.Pc_xMap[c]/ctotal[c]
+
         print "Finished Learning."
 
 
-    def _learn(self,trainMatrix,trainClasses):   # overloaded learn() for tune
+    def _learn(self,trainMatrix,trainClasses,smooth_rate):   # overloaded learn() for tune
         print "\nLearning ..."
         numTrainDocs = len(trainMatrix)
         numWords = len(trainMatrix[0])
         for c in set(trainClasses):
             self.PcMap[c] = (trainClasses.count(c))/float(numTrainDocs)
-            self.Pc_xMap[c] = ones(numWords)    # ones(): return a array with elements of float type
+            #////////////////////////////////////////////////////////////////////
+            self.Pc_xMap[c] =  array([smooth_rate]*numWords)    
+            #////////////////////////////////////////////////////////////////////
         
         ctotal = {}                             # record the total none zero feature number of samples for each class
         for i in range(numTrainDocs):
@@ -129,9 +133,20 @@ class NaiveBayes(object):
         result = sorted(result.iteritems(), key=operator.itemgetter(1),reverse=True)
         return result[0][0]                           # sorted result is in type like [(c1, p1),(c2,p2)...]
 
+    def _classify(self,vec2Classify,trainClasses):
+        result = {}
+        for c in set(trainClasses):
+            for  v in (vec2Classify * self.Pc_xMap[c]):
+                if(v>0):
+                    result[c] = result.get(c,0.) + log(v) 
+            result[c] = result.get(c,0.) + log(self.PcMap[c])
+        import operator
+        result = sorted(result.iteritems(), key=operator.itemgetter(1),reverse=True)
+        return result[0][0] 
+
 
     # -------------------- Use the model and context features to predict the word sense ---------------------
-    def predict(self,trainDir,testDir,dirOut):
+    def predict(self,trainDir,testDir,dirOut,smooth_rate=0.001):
         names = util.readNames(testDir+"namefile")
         if(0==len(names)):
             names = util.readDir(testDir)
@@ -145,7 +160,7 @@ class NaiveBayes(object):
             self.infile = infile
             self.loadFeature()
             self.createVocabList()
-            self.learn()
+            self.learn(smooth_rate)
 
             self.infile = testDir + name
             self.loadFeature("test")
@@ -161,43 +176,56 @@ class NaiveBayes(object):
 
 
     # ------------ Randomly fold the trainSet to get train and test set,and test the error rate -------------
-    def tune(self,n):
+    def tune(self,n,smooth_rate):
         self.loadFeature()                  
         self.createVocabList()
 
         length = len(self.docList)
-        tuneSet_length = int(length/n)
-        trainingSet = range(length);
-        tuneSet=[]                                                       # create test set of index
-        for i in range(tuneSet_length):
-            randIndex = int(random.uniform(0,len(trainingSet)))
-            tuneSet.append(trainingSet[randIndex])
-            del(trainingSet[randIndex])  
+        print "Total length = ",length
+        scale = range(length);
+        tuneSet = []
+        trainSet = []       
+        for c in set(self.classVec):
+            cNum = self.classVec.count(c)
+            selectNum = cNum/n
+            if(0==selectNum):
+                selectNum = 1  
+            cSet = []                                              # create test set of index
+            for i in scale:
+                if(c==self.classVec[i]):
+                    cSet.append(i)
+            random.shuffle(cSet)
+            tuneSet.extend(cSet[0:selectNum])
+
+        print "TuneSet length = ",len(tuneSet)
+        for i in scale:
+            if i not in tuneSet:
+                trainSet.append(i)
 
         trainMat=[]; trainClasses = []
-        for docIndex in trainingSet:                                     # train the classifier (get probs) trainNB0
+        for docIndex in trainSet:                                     # train the classifier (get probs) trainNB0
             trainMat.append(self.bagOfWords2Vec(self.docList[docIndex]))
             trainClasses.append(self.classVec[docIndex])
 
-        self._learn(array(trainMat),trainClasses)
+        self._learn(array(trainMat),trainClasses,smooth_rate)
 
         print "\nTesting... "
         right_count = 0.0
         for docIndex in tuneSet:
             vec2Classify = self.bagOfWords2Vec(self.docList[docIndex])
-            result = self.classify(vec2Classify)
+            result = self._classify(vec2Classify,trainClasses)
             if(result==self.classVec[docIndex]):
                 right_count = right_count + 1
         print "Finished Testing."   
 
-        right_rate = right_count/tuneSet_length
+        right_rate = right_count/len(tuneSet)
         return right_rate
 
     # ---------------------------- Repeatly test the model and get a even error rate ---------------------------
-    def Random_Cross_Validation(self,times,fold):
+    def Random_Cross_Validation(self,times,fold,smooth_rate):
         results = []
         for i in range(times):
-            results.append(self.tune(fold))
+            results.append(self.tune(fold,smooth_rate))
         right_rate = sum(results)/len(results)
         print "\nFor",times,"times",fold,"Random Cross Validation, the even right_rate is:",right_rate
         return right_rate
@@ -209,37 +237,42 @@ class NaiveBayes(object):
 
 def main():
 
-    
-   
-    extractor = Extractor()
-    extractor.extract("../corpus/train_corpus.xml", "../train/", "train", 7, 1, 1, " | ")
-    extractor.extract("../corpus/test_corpus.xml", "../test/", "test", 7, 1, 1," | ")
-    names = util.readNames("../test/namefile")
-
+    smooth_rate = 0.0001
 
     '''
+    #------------------------------For Feature Extractor------------------------------
+    extractor = Extractor()
+    extractor.extract("../corpus/train_corpus.xml", "../train/", "train", 7, 3, 2, " | ")
+    extractor.extract("../corpus/test_corpus.xml", "../test/", "test", 7, 3, 2," | ")
+    '''
+    names = util.readNames("../test/namefile")
+    
+
+    '''
+    #------------------------------For Random Validation-------------------------------
     fout = open("../result/Tune_Result "+str(time.ctime())+".csv","a")
+    results = []
     for name in names:
         infile = "../train/"+name
+
         nb = NaiveBayes(infile)
-        result = name + "," + str(nb.Random_Cross_Validation(10,20)) + "\n"
-        fout.write(result)
+        print "---------",name,"----------"
+        result = nb.Random_Cross_Validation(20,4,smooth_rate)
+        results.append(result)
+        reStr = name + "," + str(result) + "\n"
+        fout.write(reStr)
+    fout.close()
+    print "Macro AVG:",sum(results)/len(results)
 
     '''
+    #------------------------------------For Test--------------------------------------
     for name in names:
         print name
     nb2 = NaiveBayes()
-    resultfile = nb2.predict("../train/", "../test/", "../result/")
-
-    util.evaluate(resultfile,"../result/test_answer")
-
-
-
+    resultfile = nb2.predict("../train/", "../test/", "../result/",smooth_rate)
+    util.evaluate(resultfile, "../result/test_answer")
     
-
-
-
-
+    
 
 if __name__ == "__main__":
     main()
